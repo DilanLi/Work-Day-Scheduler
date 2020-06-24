d5027cb0637389eda4f75f7929dcdc723b3123fc
Dilan Li <dilanli0706@gmail.com>
2020-06-24 15:27:57 -0600

    fixed single safe button saves all content issue

--- Assets/script.js
+++ Assets/script.js
@@ -8,11 +8,11 @@ $(document).ready(function() {
     $("#currentDay").text(currentInfo);
     $("#currentTime").text(currentTime);
     
+    //this function dynamically updates hours and seconds on the html
     function updateTime(){
         currentTime = moment().format('h:mm:ss a');
         $("#currentTime").text(currentTime);
     }
-
     setInterval(updateTime, 1000);
 
     //grab value of current hour from moment.js library
@@ -36,11 +36,9 @@ displayTodo();
 
 //this function saves user input as event content and saves the events under hour of day within local storage
 function saveTodo(){
-    for (i=0; i < 9; i++){
-        var value = $("textarea").eq(i).val();
-        var hour = $(".hour").eq(i).data("hour");
+        var value = $(this).parent().siblings("textarea").val();
+        var hour = $(this).parent().siblings(".hour").data("hour");
         localStorage.setItem(hour, value);
-        }    
 }
 
 //this function grabs all saved events in local storage and displays them
